d35accb5f56311a708a42d026ef91530debf3be5
Celebal <aditusingh58@gmail.com>
2021-05-14 23:26:37 +0530

    modified forecast file

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (x,y,callback) => {
          }else if(response.body.current.length === 0){
              callback('Please try with other coordinates',undefined)
          }else{
-             callback(undefined,response.body.current.weather_descriptions)
+            callback(undefined,response.body.current.weather_descriptions[0] +'. It is currently '+response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " degrees out. The humidity is " + response.body.current.humidity + "%.")
          }
     })
 
